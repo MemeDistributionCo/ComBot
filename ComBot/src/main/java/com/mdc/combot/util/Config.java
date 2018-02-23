2ab0725599f93309e9a7db3a7d04b89f7d4fead4
Zach | xDest <destroyer7712@gmail.com>
2018-02-22 22:44:58 -0500

    Support for multi server config
    Need to do permissions still

--- ComBot/src/main/java/com/mdc/combot/util/Config.java
+++ ComBot/src/main/java/com/mdc/combot/util/Config.java
@@ -3,13 +3,14 @@ package com.mdc.combot.util;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+
+import net.dv8tion.jda.core.entities.Guild;
 
 public class Config {
 
@@ -57,23 +58,81 @@ public class Config {
 				e.printStackTrace();
 			}
 		}
-		/*
-		 * Y'all hoes best not mess with my nice Christian f i l e  f o r m a t t i n g
-		 */
-		Map<String,String> configMap = new HashMap<String,String>();
+
+		return readConfigFromFile(configFile);
+	}
+
+
+	private static Config readConfigFromFile(File f) {
 		try {
-			Scanner s = new Scanner(new FileInputStream(configFile));
+			Map<String,String> configMap = new HashMap<String,String>();
+			
+			Scanner s = new Scanner(new FileInputStream(f));
 			while(s.hasNextLine()) {
 				String ln = s.nextLine();
+				if(ln.trim().startsWith("#") || ln.trim().equals("")) {
+					continue;
+				}
 				String key = ln.substring(0, ln.indexOf(':')).replace(" ", "");
 				String value = ln.substring(ln.indexOf(':')+1);
 				configMap.put(key, value);
 			}
 			s.close();
-		} catch (FileNotFoundException e) {
+			return new Config(configMap);
+		} catch (IOException e) {
 			e.printStackTrace();
+			System.out.println("Error loading " + f.getName() + " as config");
+			return null;
 		}
-		return new Config(configMap);
+	}
+	
+	/**
+	 * Checks whether this guilds <code>guildid.txt</code> config file exists in <code>./multiserver-config/</code>
+	 * @param g The guild to check
+	 * @return True, if it exists
+	 */
+	public static boolean guildHasConfig(Guild g) {
+		File f = new File(Util.BOT_SETTINGS_PATH + File.separatorChar + "multiserver-config" + File.separatorChar + g.getId() + ".txt");
+		return f.exists() && f.isFile();
+	}
+	
+	/**
+	 * Create a configuration file for a guild in <code>./multiserver-config/guildid.txt</code>. Overwrites existing file.
+	 * @param g The guild
+	 */
+	public static void createConfigForGuild(Guild g) {
+		File f = new File(Util.BOT_SETTINGS_PATH + File.separatorChar + "multiserver-config" + File.separatorChar + g.getId() + ".txt");
+		if(!f.exists()) {
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		createDefaultConfigFile(f);
+	}
+
+	public static Map<String, Config> getMulticonfigMap() {
+		String path = Util.BOT_SETTINGS_PATH;
+		File configDirectory = new File(path + File.separatorChar + "multiserver-config");
+		if(!configDirectory.exists()) {
+			configDirectory.mkdirs();
+			return new HashMap<String,Config>();
+		}
+		/*
+		 * Y'all hoes best not mess with my nice Christian f i l e  f o r m a t t i n g
+		 */
+		Map<String,Config> multiMap = new HashMap<String,Config>();
+		for(File confFile : configDirectory.listFiles()) {
+			long guildId = Long.parseLong(confFile.getName().replace(".txt", ""));
+			Config guildConfig = readConfigFromFile(confFile);
+			if(guildConfig!=null) {
+				multiMap.put(""+guildId, guildConfig);
+			}
+		}
+				
+		
+		return multiMap;
 	}
 	
 	
