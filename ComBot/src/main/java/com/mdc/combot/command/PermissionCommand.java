ad0618f20027506e835284ff12ad929b9c141792
Zach | xDest| <destroyer7712@gmail.com>
2019-08-20 13:25:33 -0400

    Small progress

--- ComBot/src/main/java/com/mdc/combot/command/PermissionCommand.java
+++ ComBot/src/main/java/com/mdc/combot/command/PermissionCommand.java
@@ -2,6 +2,7 @@ package com.mdc.combot.command;
 
 import com.mdc.combot.ComBot;
 import com.mdc.combot.permissions.DefaultPermissionManager;
+import com.mdc.combot.util.Util;
 
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
@@ -16,17 +17,34 @@ public class PermissionCommand implements Command {
 	public void called(ComBot bot, MessageReceivedEvent e) {
 		if(bot.memberHasPerm("bot.permission", e.getMember())) {
 			//perm <add/remove> <role/user/everyone> <local/global> [role/@user] <perm>
-			String fullMsg = e.getMessage().getContentRaw();
-			String[] args = fullMsg.split(" ");
+			String[] args = Util.getArgsFromCommand(this, e);
 			String action = args[0];
 			String target = args[1];
 			String level = args[2];
 			if(args.length == 4) {
 				//Everyone
 				String perm = args[3];
-				
 				if(action.equalsIgnoreCase("add")) {
 					//this might take more work than I was expecting whoops
+					if(level.equalsIgnoreCase("local")) {
+						if(target.equalsIgnoreCase("role")) {
+							
+						} else if (target.equalsIgnoreCase("user")) {
+							
+						} else {
+							
+						}
+					} else if (level.equalsIgnoreCase("global")) {
+						if(target.equalsIgnoreCase("role")) {
+							//
+						} else if (target.equalsIgnoreCase("user")) {
+							
+						} else {
+							
+						}
+					} else {
+						e.getTextChannel().sendMessage("Invalid syntax, only allowed perm levels are `local` and `global`").queue();
+					}
 				} else if (action.equalsIgnoreCase("remove")) {
 					
 				} else {
